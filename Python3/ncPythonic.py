7fb0142987cbb571e1839c46d6a68c32989099e4
Breno Santana Bruno <brenosantana@Brenos-MacBook-Pro.local>
2023-06-10 18:45:14 -0300

    Upload nc_pythonic
    Signed-off-by: Breno Santana Bruno <brenosantana@Brenos-MacBook-Pro.local>

---
+++ Python3/ncPythonic.py
@@ -0,0 +1,88 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_ouput(shlex.split(cmd),
+                                    stderr=subprocess.STDOUT)
+    return output.decode()
+
+
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+            
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except:
+
+                    print('User terminated.')
+                    self.socket.close()
+                    sys.exit()
+        
+def listen(self):
+    self.socket.bind((self.args.target, self.args.port))
+    self.socket.listen(5)
+    
+    while True:
+        client_socket, _ = self.socket.accept()
+        client_thread = threading.Thread(
+        target=self.handle, args=(client_socket,)
+        )
+        client_thread.start()
+                    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'nc_pythonic',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog = textwrap.dedent('''Example:
+            nc_pythonic -t 192.168.0.1 -p 555 -l -c # command shell
+            nc_pythonic -t 192.168.0.1 -p 555 -l -u=mytest.txt # Upload to file
+            nc_pythonic -t 192.168.0.1 -p 555 -l -e =\""at /etc/passwd\" # execute command
+            echo 'ABC'| ./nc_pythonic -t 192.168.0.1 =p 135 # echo text to server port 135
+            nc_pythonic -t 192.168.0.1 -p 5555 # connect to server
+            '''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add.argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, defeault=5555, help='specified IP')
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+        
+    nc_pythonic = NetCat(args, buffer.encode())
+    nc_pythonic.run()
